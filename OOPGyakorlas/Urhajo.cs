e60c9371f692ff39e44cf1d3eae8faab57ba4fcd
Braun Roland <rolibraun94@gmail.com>
2025-02-07 12:29:28 +0100

    ŰrhajóOOP kész (tényleg)

--- OOPGyakorlas/Urhajo.cs
+++ OOPGyakorlas/Urhajo.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace OOPGyakorlas
 {
-    internal class Urhajo
+    public class Urhajo
     {
+        private string nev;
+        private int sebesség;
+        private int utasKapacítás;
+        private int uzemanyagSzint;
+
+        public Urhajo(string nev, int sebesség, int utasKapacítás, int uzemanyagSzint)
+        {
+            this.nev = nev;
+            this.sebesség = sebesség;
+            this.utasKapacítás = utasKapacítás;
+            this.uzemanyagSzint = uzemanyagSzint;
+        }
+
+        public Urhajo(string nev, int utasKapacítás)
+        {
+            this.nev = nev;
+            sebesség = 0;
+            this.utasKapacítás = utasKapacítás;
+            uzemanyagSzint = 100;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Sebesség { get => sebesség; set => sebesség = value; }
+        public int UtasKapacítás { get => utasKapacítás; set => utasKapacítás = value; }
+        public int UzemanyagSzint { get => uzemanyagSzint; set => uzemanyagSzint = value; }
+
+        public void Indulas()
+        {
+            sebesség += 5;
+            uzemanyagSzint -= 10;
+        }
+
+        public void Mennyiseg(int mennyiseg)
+        {
+            uzemanyagSzint += mennyiseg;
+        }
+
+        public void landolás()
+        {
+            sebesség = 0;
+        }
+
+        public override string? ToString()
+        {
+            return $"{nev}: sebssség: {sebesség} | Kapacítás: {UtasKapacítás} | Üzemanyag: {uzemanyagSzint}%";
+        }
     }
 }
